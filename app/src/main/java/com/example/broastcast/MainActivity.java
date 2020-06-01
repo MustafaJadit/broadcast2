d2bf5c0238e1afea3e1fbf74e2c3a896ce61e76e
mustafaatush <mustafaatush@gmail.com>
2020-05-31 17:54:32 -0700

    first commit

--- app/src/main/java/com/example/broastcast/MainActivity.java
+++ app/src/main/java/com/example/broastcast/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.broastcast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    MyReceiver2 receiver;
+    IntentFilter intentFilter;
+
+    //we have two ways to send broastcast
+//    1. register dynamically
+//    2. explicitly denote intent with action
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        receiver = new MyReceiver2();
+//        intentFilter = new IntentFilter("com.journaldev.broadcastreceiver.SOME_ACTION");
+        intentFilter = new IntentFilter("com.example.broastcast.action");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        registerReceiver(receiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+//        unregisterReceiver(receiver);
+    }
+
+    @OnClick(R.id.button)
+    void someMethod() {
+
+//        Intent intent = new Intent("com.journaldev.broadcastreceiver.SOME_ACTION");
+        Intent intent = new Intent("com.example.broastcast.action");
+        // we should use explicit intent
+//        Intent intent = new Intent(this, MyReceiver2.class);
+//        intent.setAction("com.example.broastcast.action");
+
+        sendBroadcast(intent);
     }
 }
---
+++ app/src/main/java/com/example/broastcast/MyReceiver2.java
@@ -0,0 +1,15 @@
+package com.example.broastcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver2 extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "MyReceiver2 is received", Toast.LENGTH_LONG).show();
+
+    }
+}
